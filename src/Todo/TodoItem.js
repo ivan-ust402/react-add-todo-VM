15435a094184588129fbaa9a82e3b558931f67d2
Ivan Ustyantsev <vangold915@gmail.com>
2022-10-27 18:51:50 +0700

    Used useContext hook and added removeTodo handler to button element

--- src/Todo/TodoItem.js
+++ src/Todo/TodoItem.js
@@ -1,5 +1,6 @@
-import React from "react"
+import React, { useContext } from "react"
 import PropTypes from "prop-types"
+import Context from "../context"
 
 const styles = {
   li: {
@@ -16,14 +17,15 @@ const styles = {
   },
 }
 function TodoItem({ todo, index, onChange }) {
+  const { removeTodo } = useContext(Context)
   const classes = []
 
   if (todo.completed) {
-    classes.push('done')
+    classes.push("done")
   }
   return (
     <li style={styles.li}>
-      <span className={classes.join(' ')}>
+      <span className={classes.join(" ")}>
         <input
           type="checkbox"
           checked={todo.completed}
@@ -35,7 +37,8 @@ function TodoItem({ todo, index, onChange }) {
         {todo.title}
       </span>
 
-      <button className="rm">&times;</button>
+      {/* <button className="rm" onClick={() => removeTodo(todo.id)}>&times;</button> */}
+      <button className="rm" onClick={removeTodo.bind(null, todo.id)}>&times;</button>
     </li>
   )
 }
