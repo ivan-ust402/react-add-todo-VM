9d71174847bcd1d48649ca7ecd693a57aad14379
Ivan Ustyantsev <vangold915@gmail.com>
2022-10-27 14:43:52 +0700

    Created TodoList component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import React from "react";
+import TodoList from "./Todo/TodoList";
 
 function App() {
   return (
-    <div className="wrapper" />
+    <div className="wrapper">
+      <h1>React tutorial</h1>
+      <TodoList />
+    </div>
   )
 }
 
