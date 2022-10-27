34ec1fc58f875bcbb0db3f78d2593a18f4a585a2
Ivan Ustyantsev <vangold915@gmail.com>
2022-10-27 18:37:11 +0700

    Added event handler for checkbox

--- src/Todo/TodoItem.js
+++ src/Todo/TodoItem.js
@@ -1,38 +1,49 @@
-import React from "react";
-import PropTypes from 'prop-types'
+import React from "react"
+import PropTypes from "prop-types"
 
 const styles = {
-    li: {
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        padding: '.5rem 1rem',
-        border: '1px solid #ccc',
-        borderRadius: '4px',
-        marginBottom: '.5rem'
-    },
-    input: {
-        marginRight: '1rem',
-    }, 
+  li: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: ".5rem 1rem",
+    border: "1px solid #ccc",
+    borderRadius: "4px",
+    marginBottom: ".5rem",
+  },
+  input: {
+    marginRight: "1rem",
+  },
 }
-function TodoItem({ todo, index }) {
-    return (
-        <li style={styles.li}>
-            <span>
-                <input type="checkbox" style={styles.input} />
-                <strong>{index + 1}.</strong>
-                &nbsp;
-                {todo.title}
-            </span>
+function TodoItem({ todo, index, onChange }) {
+  const classes = []
 
-            <button className="rm">&times;</button>
-        </li>
-    )
+  if (todo.completed) {
+    classes.push('done')
+  }
+  return (
+    <li style={styles.li}>
+      <span className={classes.join(' ')}>
+        <input
+          type="checkbox"
+          checked={todo.completed}
+          style={styles.input}
+          onChange={() => onChange(todo.id)}
+        />
+        <strong>{index + 1}.</strong>
+        &nbsp;
+        {todo.title}
+      </span>
+
+      <button className="rm">&times;</button>
+    </li>
+  )
 }
 
 TodoItem.propTypes = {
-    todo: PropTypes.object.isRequired,
-    index: PropTypes.number
+  todo: PropTypes.object.isRequired,
+  index: PropTypes.number,
+  onChange: PropTypes.func.isRequired,
 }
 
 export default TodoItem
