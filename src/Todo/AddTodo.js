05820d583dae1a3706803ae4eb2249b2a55b6620
Ivan Ustyantsev <vangold915@gmail.com>
2022-10-27 20:14:02 +0700

    Added some styles for AddTodo component

--- src/Todo/AddTodo.js
+++ src/Todo/AddTodo.js
@@ -1,10 +1,33 @@
 import React from "react";
 
+const styles = {
+    form: {
+        marginBottom: '1rem',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: "center",
+        padding: ".5rem 1rem",
+        border: "1px solid #ccc",
+        borderRadius: "4px",
+    },
+    input: {
+        minWidth: '70%',
+        padding: ".5rem 1rem",
+        border: "1px solid #ccc"
+    },
+    button: {
+        padding: ".5rem 1rem",
+        border: "1px solid #ccc",
+        background: '#6dd319',
+        color: 'white',
+    }
+}
+
 function AddTodo() {
     return (
-        <form>
-            <input type="text" />
-            <button type="submit">Add todo</button>
+        <form style={styles.form}>
+            <input type="text" style={styles.input} />
+            <button type="submit" style={styles.button}>Add todo</button>
         </form>
     )
 }
