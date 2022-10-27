13cf880bc054d6220c91c191f5f2b8277b4e40a7
Ivan Ustyantsev <vangold915@gmail.com>
2022-10-27 23:10:50 +0700

    Showing lazy loading

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import TodoList from "./Todo/TodoList"
 import Context from "./context"
 import Loader from "./Loader"
 
-const AddTodo = React.lazy(() => import('./Todo/AddTodo'))
+const AddTodo = React.lazy(() => new Promise(resolve => {
+  setTimeout(() => {
+    resolve(import('./Todo/AddTodo'))
+  }, 3000)
+}))
 
 function App() {
   const [todos, setTodos] = React.useState([
@@ -51,7 +55,7 @@ function App() {
     <Context.Provider value={{removeTodo: removeTodo}}>
       <div className="wrapper">
         <h1>Your Todo List</h1>
-        <React.Suspense fallback={<p>Loading...</p>}>
+        <React.Suspense fallback={<Loader />}>
           <AddTodo onCreate={addTodo} />
         </React.Suspense>
         {loading && <Loader />}
